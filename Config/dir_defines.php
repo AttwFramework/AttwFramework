16e9d3ade30a7edccf1ffec648917e00ea0d51b3
Server <server@server.com>
2014-04-13 11:28:20 -0300

    Add ATTW const

--- Config/dir_defines.php
+++ Config/dir_defines.php
@@ -1,11 +1,11 @@
 <?php
-    define( 'DS', DIRECTORY_SEPARATOR );
-    define( 'APP_ROOT', realpath( __DIR__  . '..' . DS . '..' . DS ) );
-    define( 'LIBS', APP_ROOT . DS . 'Libraries' );
-    define( 'APP', APP_ROOT . DS . 'Application' );
-
+    ( !defined( 'DS' ) ) ? define( 'DS', DIRECTORY_SEPARATOR ) : null;
+    ( !defined( 'APP_ROOT' ) ) ? define( 'APP_ROOT', realpath( __DIR__  . '..' . DS . '..' . DS ) ) : null;
+    ( !defined( 'LIBS' ) ) ? define( 'LIBS', APP_ROOT . DS . 'Libraries' ) : null;
+    ( !defined( 'APP' ) ) ? define( 'APP', APP_ROOT . DS . 'Application' ) : null;
+    ( !defined( 'ATTW' ) ) ? define( 'ATTW', LIBS . DS . 'Attw' ) : null;
     
-    define( 'PUBLIC_FILES', APP_ROOT . DS . 'public' );
-    define( 'IMAGES', PUBLIC_FILES . DS . 'images' );
-    define( 'CSS', PUBLIC_FILES . DS . 'css' );
-    define( 'JS', PUBLIC_FILES . DS . 'js' );
+    ( !defined( 'PUBLIC_FILES' ) ) ? define( 'PUBLIC_FILES', APP_ROOT . DS . 'public' ) : null;
+    ( !defined( 'IMAGES' ) ) ? define( 'IMAGES', PUBLIC_FILES . DS . 'images' ) : null;
+    ( !defined( 'CSS' ) ) ? define( 'CSS', PUBLIC_FILES . DS . 'css' ) : null;
+    ( !defined( 'JS' ) ) ? define( 'JS', PUBLIC_FILES . DS . 'js' ) : null;
